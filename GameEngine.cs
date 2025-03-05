c7f9d5d8d151accd253819c64f27263d2d4b8e93
Kyle S <kstephenson1@my.wctc.edu>
2025-03-04 20:32:09 -0600

    Implemented CommandHandler for dependancy decoupling

--- GameEngine.cs
+++ GameEngine.cs
@@ -1,23 +1,20 @@
 ﻿using Spectre.Console;
 using w6_assignment_ksteph.Commands;
-using w6_assignment_ksteph.Commands.ItemCommands;
-using w6_assignment_ksteph.Commands.UnitCommands;
-using w6_assignment_ksteph.DataHelper;
 using w6_assignment_ksteph.Entities;
 using w6_assignment_ksteph.Interfaces;
-using w6_assignment_ksteph.Interfaces.CharacterBehaviors;
-using w6_assignment_ksteph.Items.WeaponItems;
 using w6_assignment_ksteph.UI;
 
 namespace w6_assignment_ksteph;
 
 public class GameEngine
 {
+    private CommandHandler _commandHandler;
     private UnitManager _unitManager;
     private UserInterface _userInterface;
 
-    public GameEngine(UnitManager unitManager, UserInterface userInterface)
+    public GameEngine(UnitManager unitManager, UserInterface userInterface, CommandHandler commandHandler)
     {
+        _commandHandler = commandHandler;
         _unitManager = unitManager;
         _userInterface = userInterface;
     }
@@ -46,99 +43,23 @@ public class GameEngine
     public void Run()
     {
         // Shows the main menu.  Allows you to add/edit characters before the game is started.
-        _userInterface.MainMenu.Display();
-
-        // Builds the unit select menu.
-        _userInterface.MainMenu.Update();
-        _userInterface.UnitSelectionMenu.Update();
+        _userInterface.MainMenu.Display("[[Start Game]]");
 
         while (true)
         {
             // Asks the user to choose a unit.
-            IEntity unit1 = _userInterface.UnitSelectionMenu.Display("Select unit to control");
-            if (unit1 == null) break;
+            IEntity unit = _userInterface.UnitSelectionMenu.Display("Select unit to control", "[[Exit Game]]");
+            if (unit == null) break;
 
             // If the selected unit is down, restarts
-            if (unit1.HitPoints <= 0) continue;
+            if (unit.HitPoints <= 0) continue;
 
             // Asks the user to choose an action for unit.
-            ICommand command = _userInterface.CommandMenu.Display(unit1, $"Select action for {unit1.Name}");
+            ICommand command = _userInterface.CommandMenu.Display(unit, $"Select action for {unit.Name}", "[[Go Back]]");
             if (command == null) continue; // Go back was selected, sends back to unit selection.
 
-            // If the unit is able to move, the unit moves.
-            if (command.GetType() == typeof(MoveCommand))
-            {
-                unit1.Move();
-            }
-
-            // If the unit has a usable item, it can use an item.
-            else if (command.GetType() == typeof(UseItemCommand))
-            {
-                if (unit1.Inventory.Items!.Count > 0)
-                {
-                    // Shows a list of items that are in the selected unit's inventory and asks the user to select an item.
-                    IItem item = _userInterface.InventoryMenu.Display(unit1, $"Select item for {unit1.Name}.");
-
-                    // Item is null if the user selects go back.
-                    if ( item != null)
-                    {
-                        // Checks the items to see what commands are allowed, displays those commands to the user and asks for a selection
-                        ICommand itemCommand = _userInterface.ItemCommandMenu.Display(item, $"Select action for {unit1.Name} to use on {item.Name}");
-
-                        // Command is null if the user selects "Go Back"
-                        if ( itemCommand != null ) 
-                        {
-                            // The selected command is executed by the selected unit.
-                            switch (itemCommand) 
-                            {
-                                case EquipCommand:
-                                    unit1.Equip((item as WeaponItem)!);
-                                    break;
-                                case UseItemCommand:
-                                    unit1.UseItem(item);
-                                    break;
-                                case TradeItemCommand:
-                                    IEntity tradeTarget = _userInterface.UnitSelectionMenu.Display($"Select unit to trade {item} to.");
-                                    unit1.TradeItem(item, tradeTarget);
-                                    break;
-                                case DropItemCommand:
-                                    unit1.DropItem(item);
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"{unit1.Name} has no usable items!");
-                }
-
-            }// If the unit is able to attack, it attacks.
-            else if (command.GetType() == typeof(AttackCommand))
-            {
-                if (unit1 is IAttack)
-                {
-                    IEntity unit2 = _userInterface.UnitSelectionMenu.Display($"Select unit being attacked by {unit1.Name}");
-                    unit1.Attack(unit2);
-                }
-            }
-            // If the unit is able to heal, it heals.
-            else if (command.GetType() == typeof(HealCommand))
-            {
-                IEntity unit2 = _userInterface.UnitSelectionMenu.Display($"Select unit being healed by {unit1.Name}");
-                ((IHeal)unit1).Heal(unit2);
-            }
-            // If the unit is able to cast spells, it casts a spell.
-            else if (command.GetType() == typeof(CastCommand))
-            {
-                string spell = Input.GetString($"Enter name of spell being cast by {unit1.Name}: ");
-                ((ICastable)unit1).Cast(spell);
-            }
-
-            // Rebuilds the unit selection menu so that the health bars are updated.
-            _userInterface.UnitSelectionMenu.Update();
+            // Takes the command and the unit and handles
+            _commandHandler.HandleCommand(command, unit);
 
             // Waits for user input.  Escape leaves the program and any other button loops the process.
             AnsiConsole.MarkupLine($"\nPress [green][[ANY KEY]][/] to continue...");
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using w6_assignment_ksteph.Commands;
 using w6_assignment_ksteph.Entities;
 using w6_assignment_ksteph.Entities.Abstracts;
 using w6_assignment_ksteph.FileIO;
@@ -16,11 +17,12 @@ class Program
         services.AddTransient<UserInterface>();
         services.AddTransient<CharacterUtilities>();
         services.AddTransient<CharacterUI>();
+        services.AddTransient<CommandHandler>();
         services.AddTransient<CommandMenu>();
-        services.AddTransient<InventoryMenu>();
-        services.AddTransient<ItemCommandMenu>();
         services.AddTransient<ExitMenu>();
         services.AddSingleton<FileManager<UnitBase>>();
+        services.AddTransient<InventoryMenu>();
+        services.AddTransient<ItemCommandMenu>();
         services.AddTransient<MainMenu>();
         services.AddSingleton<UnitClassMenu>();
         services.AddSingleton<UnitManager>();
@@ -30,8 +32,9 @@ class Program
 
         UnitManager unitManager = provider.GetRequiredService<UnitManager>();
         UserInterface userInterface = provider.GetRequiredService<UserInterface>();
+        CommandHandler commandHandler = provider.GetRequiredService<CommandHandler>();
 
-        GameEngine engine = new GameEngine(unitManager, userInterface);
+        GameEngine engine = new GameEngine(unitManager, userInterface, commandHandler);
         engine.StartGameEngine();
     }
 }
